038cc4c1ddfc9dedd7dd76288947a60d8a310380
Sadiksha Gautam <sadiksha.gautam@gmail.com>
2017-03-29 19:48:26 +0200

    Adds controller and views for index, new and create tasks.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,21 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+	def create
+		@task = Task.new(task_params)
+		@task.save
+		redirect_to action: "index"
+	end
+
+ private
+
+  def task_params
+    params.require(:task).permit(:name, :description, :creator, :motivator, :credit)
+  end
 end
